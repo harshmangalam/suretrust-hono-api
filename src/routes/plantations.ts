73857e8f1db3e6bf897c4d4e1e2079c1c291b411
harshmangalam <harshdev8218@gmail.com>
2023-12-22 23:32:30 +0530

    create plant

--- src/routes/plantations.ts
+++ src/routes/plantations.ts
@@ -19,4 +19,45 @@ app.get("/stats", async (c) => {
   }
 });
 
+app.get("/charts", async (c) => {
+  try {
+    const data = await Plantations.aggregate([
+      {
+        $group: {
+          _id: "$course",
+          plantsCount: { $sum: "$plants" },
+          users: { $push: "$user" },
+        },
+      },
+    ]);
+
+    return c.json(data);
+  } catch (error) {
+    console.log(error);
+    return c.json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/create", async (c) => {
+  try {
+    const body = await c.req.json();
+    const course = body.course || "Others";
+    const batch = body.batch || "";
+    const slug = course && batch ? `${course}-(${batch})` : "Others";
+    const data = await Plantations.create({
+      ...body,
+      course,
+      batch,
+      slug,
+    });
+
+    const saved = await data.save();
+    return c.json(saved);
+  } catch (error) {
+    return c.json({ error: "Something went wrong" });
+  }
+});
+
+app.get("");
+
 export default app;
